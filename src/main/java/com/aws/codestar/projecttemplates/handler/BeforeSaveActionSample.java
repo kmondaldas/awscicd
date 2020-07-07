ef390a3b4c303bd95830dd0d37fea28916d61911
kmondaldas <65215614+kmondaldas@users.noreply.github.com>
2020-07-07 18:14:59 +0530

    Update BeforeSaveActionSample.java

--- src/main/java/com/aws/codestar/projecttemplates/handler/BeforeSaveActionSample.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/BeforeSaveActionSample.java
@@ -15,23 +15,70 @@ public class BeforeSaveActionSample extends LifeCycleActionHandler {
 	@Override
 	public ILifeCycleObjectData beforeSave(IReltioAPI reltioAPI, ILifeCycleObjectData data) {
 		IObject object = data.getObject();
+
 		IAttributes attributes = object.getAttributes();
+		String userType = object.getUpdatedBy();
+
 		try {
-			List<IAttributeValue> status = attributes.getAttributeValues("Status");
-			ISimpleAttributeValue statusVal = (ISimpleAttributeValue) status.get(0);
-			System.out.println("statusVal::::::::::::::" +statusVal);
-			String statusNew = "Pending";
-			ISimpleAttributeValue newValue = attributes.createSimpleAttributeValue("Status").value(statusNew).build();
-			attributes.addAttributeValue(newValue);
-			reltioAPI.logInfo("Status changed to Pending");
+
+			if (null != userType && !userType.equalsIgnoreCase("PDM_Batch_User")) {
+
+				if (null != attributes.getAttributeValues("ValidationStatus")
+
+						&& !attributes.getAttributeValues("ValidationStatus").isEmpty()) {
+
+					List<IAttributeValue> status = attributes.getAttributeValues("ValidationStatus");
+
+					List<IAttributeValue> errorMsg = attributes.getAttributeValues("ErrorMessage");
+
+					ISimpleAttributeValue statusVal = (ISimpleAttributeValue) status.get(0);
+
+					ISimpleAttributeValue validationErrorMsg = (ISimpleAttributeValue) errorMsg.get(0);
+
+					statusVal.setValue("Pending");
+
+					validationErrorMsg.setValue("  ");
+
+					reltioAPI.logInfo("BeforeSaveActionProvRecordValidationStatus:Existing status changed to Pending");
+
+				} else {
+
+					String statusNew = "Pending";
+
+					String errMsgNew = "  ";
+
+					ISimpleAttributeValue newValue = attributes.createSimpleAttributeValue("ValidationStatus")
+
+							.value(statusNew).build();
+
+					ISimpleAttributeValue newErrMsg = attributes.createSimpleAttributeValue("ErrorMessage")
+
+							.value(errMsgNew).build();
+
+					attributes.addAttributeValue(newValue);
+
+					attributes.addAttributeValue(newErrMsg);
+
+					object.getCrosswalks().getCrosswalks().get(0).getAttributes().addAttribute(newValue);
+
+					object.getCrosswalks().getCrosswalks().get(0).getAttributes().addAttribute(newErrMsg);
+
+					reltioAPI.logInfo("BeforeSaveActionProvRecordValidationStatus:Status changed to Pending");
+
+				}
+
+			}
+
 			return data;
 
 		} catch (Exception ex) {
+
 			reltioAPI.logError(ex.getMessage());
-			throw new RuntimeException("BeforeSaveActionSample: LCA invocation failed.");
+
+			throw new RuntimeException("BeforeSaveActionProvRecordValidationStatus: LCA execution failed.", ex);
+
 		}
 
 	}
 
-
-}  
+}
